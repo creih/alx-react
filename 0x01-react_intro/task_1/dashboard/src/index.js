1db9c380db7c1091aa5199b198f854b1cf9567a1
creih <iherr.9@gmail.com>
2024-08-22 23:38:37 +0200

    task 2 done but not yet functional

---
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -0,0 +1,17 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import { Notifications } from './Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div id="root-app">
+      <App />
+    </div>
+    <div id="root-notifications">
+      {/* <Notifications /> */}
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
