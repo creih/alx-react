68e56d1d4b6e4c0f7cf0a19db6dce2a85655a2e9
creih <iherr.9@gmail.com>
2024-08-26 21:27:41 +0200

    task 2 dine

---
+++ 0x02-react_props/task_1/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './holberton-logo.jpg'; // Make sure the path to the logo is correct
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
