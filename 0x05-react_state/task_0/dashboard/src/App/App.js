e7db85e9365fc857707bff0f33f4cba0ce9b1564
creih <iherr.9@gmail.com>
2024-09-19 13:47:15 +0200

    made class inside the App.js

--- 0x05-react_state/task_0/dashboard/src/App/App.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './holberton-logo.jpg'; // Make sure the path to the logo is correct
+import React, { Component } from 'react';
+import logo from './holberton-logo.jpg';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
@@ -7,27 +7,47 @@ import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-        <Header />
-      </header>
-      <body className="App-body">
-        <p>Login to access the full dashboard</p>
-        <Login />
-        <Notifications />
-        <CourseList />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false,
+    };
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+  }
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
 
-      </body>
-      <Footer />
-      <footer className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
-      </footer>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+          <Header />
+        </header>
+        <body className="App-body">
+          <p>Login to access the full dashboard</p>
+          <Login />
+          <Notifications 
+            displayDrawer={this.state.displayDrawer}
+            handleDisplayDrawer={this.handleDisplayDrawer}
+            handleHideDrawer={this.handleHideDrawer}
+          />
+          <CourseList />
+        </body>
+        <footer className="App-footer">
+          <p>Copyright 2020 - holberton School</p>
+          <Footer />
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
