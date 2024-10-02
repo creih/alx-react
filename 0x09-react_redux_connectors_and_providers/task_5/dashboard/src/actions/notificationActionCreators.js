cfb4bfaa10956f231e0503873a943addc20ec1fe
creih <iherr.9@gmail.com>
2024-10-02 17:26:37 +0200

    up to 18

---
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,29 @@
+import { SET_LOADING_STATE, FETCH_NOTIFICATIONS_SUCCESS } from './notificationActionTypes';
+import { SET_LOADING_STATE, FETCH_NOTIFICATIONS_SUCCESS } from './notificationActionTypes';
+
+export const setLoadingState = (isLoading) => ({
+    type: SET_LOADING_STATE,
+    isLoading,
+});
+
+export const setNotifications = (data) => ({
+    type: FETCH_NOTIFICATIONS_SUCCESS,
+    notifications: data,
+});
+
+export const fetchNotifications = () => {
+    return (dispatch) => {
+        dispatch(setLoadingState(true));
+
+        return fetch('/notifications.json')
+            .then((response) => response.json())
+            .then((data) => {
+                dispatch(setNotifications(data));
+                dispatch(setLoadingState(false));
+            })
+            .catch((error) => {
+                console.error('Error fetching notifications:', error);
+                dispatch(setLoadingState(false));
+            });
+    };
+};
