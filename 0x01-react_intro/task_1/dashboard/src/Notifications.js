e84fd2874241d648bf52c7c074503ebba6162346
creih <iherr.9@gmail.com>
2024-12-08 20:49:15 +0200

    fixed issues in task 1

--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -1,15 +1,13 @@
 // App.js
 import React from 'react';
-import logo from './holberton-logo.jpg';
-import { getFullYear, getFooterCopy } from './utils';
+// import logo from './holberton-logo.jpg';
+// import { getFullYear, getFooterCopy } from './utils';
 import './Notifications.css';
 
-function Notifications() {
+export default function Notifications() {
   return (
     <div className="Notifications">
       <p>Here is the list of notifications</p>
     </div>
   );
 }
-
-export default Notifications;
--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Notifications } from './Notifications';
+import Notifications from './Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
